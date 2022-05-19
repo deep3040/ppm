572fb76a42caf97e819078f963da8d05bef894e6
deep <patel.deep0731@gmail.com>
2022-05-19 18:40:11 +0530

    urls updated

--- ppm/aadhar/urls.py
+++ ppm/aadhar/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path('address/<int:pk>/', UpdateDestroyAddressView.as_view(), name='Address Update & Delete'),
     path('qualification/',QualificationAPI.as_view(), name='Aadhar Details'),
     path('qualification/<int:pk>/',UpdateDestroyQualificationView.as_view(),name='All Qualification'),
-    path('bank/',BankAPI.as_view(), name='Aadhar Details'),
-    path('bank/<int:pk>/',UpdateDestroyBankView.as_view(),name='Qaulification Details'),
-    path('perdet/',PerDetAPI.as_view(), name='Aadhar Details'),
-    path('perdet/<int:pk>/',UpdateDestroyPerDetAPIView.as_view(),name='Qaulification Details'),
+    path('bank/',BankAPI.as_view(), name='bank'),
+    path('bank/<int:pk>/',UpdateDestroyBankView.as_view(),name='bankupdate'),
+    path('perdet/',PerDetAPI.as_view(), name='perdet'),
+    path('perdet/<int:pk>/',UpdateDestroyPerDetAPIView.as_view(),name='perdet'),
 ]
